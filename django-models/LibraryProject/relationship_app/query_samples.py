d34d18d204c7b5e25a44368b3f9c0c5692b60728
onychi <onyt251@gmail.com>
2025-09-07 23:46:09 +0300

    edit query sample library class

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,7 +8,7 @@ for book in books_by_author:
     print(book.title)
 
 # Example: List all books in a library
-library = Library.objects.get(name="Central Library")
+library = Library.objects.get(name="library_name")
 books_in_library = library.books.all()
 for book in books_in_library:
     print(book.title)
