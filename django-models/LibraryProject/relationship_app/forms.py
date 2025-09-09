3892cbc32fe535a3f336209bd552d5a61b07e3f1
onychi <onyt251@gmail.com>
2025-09-09 14:53:07 +0300

    created a forms.py and configured the views.py

---
+++ django-models/LibraryProject/relationship_app/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date', 'isbn']
+        
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date', 'isbn']
