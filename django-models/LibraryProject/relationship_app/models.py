3cf9d3a4050413e131c2d510b2f8ee56b9c2ef6c
onychi <onyt251@gmail.com>
2025-09-09 14:23:16 +0300

    Implementing Custom Permissions in Django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -15,12 +15,12 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
-class Meta:
-    premissions = [
-        ("can_add_book", "Can add book"),
-        ("can_change_book", "Can change book"),
-        ("can_delete_book", "Can delete book"),
-    ]
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
