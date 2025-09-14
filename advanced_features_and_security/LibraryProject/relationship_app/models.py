dd2e3ed7bd0dcf0f78ef6892eada0fb04a65d7e2
onychi <onyt251@gmail.com>
2025-09-15 00:27:41 +0300

    django assignments

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -19,10 +19,10 @@ class Book(models.Model):
 
     class Meta:
         permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-            ("can_view_book", "Can view book"),
+            ("can_add", "Can add"),
+            ("can_change", "Can change"),
+            ("can_delete", "Can delete"),
+            ("can_view", "Can view"),
         ]
 
     def __str__(self):
