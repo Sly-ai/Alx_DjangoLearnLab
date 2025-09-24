8d81d665c06f14162edce498f759f38ca69cffa8
onychi <onyt251@gmail.com>
2025-09-24 18:33:16 +0300

    Building my  First API Endpoint with Django REST Framework

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -3,5 +3,5 @@ from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Book
-        fields = '__all__'
+        model = Book        # link it to the Book model
+        fields = '__all__'  # include *all* fields (title, author, year)
