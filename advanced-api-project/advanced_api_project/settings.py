09d9fd9399aa0d0ff0314025b526e9cc1a2cd81f
onychi <onyt251@gmail.com>
2025-09-28 22:09:26 +0300

    installed django filter

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django.filters',
     'api',
+    'django_filters',
 ]
 
 REST_FRAMEWORK = {
