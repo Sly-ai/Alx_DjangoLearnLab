77e08a1fb222d4dede6055c703d072623eef214c
onychi <onyt251@gmail.com>
2025-09-28 21:50:33 +0300

    added from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated to views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 
 # 📖 List all books (read-only allowed for unauthenticated users)
 class BookListView(generics.ListAPIView):
